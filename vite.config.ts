9fdf8b068033e97dbe1734c6cb293770e4a8207c
Mohammadreza Babayi <m.babayi574@gmail.com>
2025-06-08 16:59:38 +0330

    Initialize Flowbite Component Library

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import TanStackRouterVite from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
-
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        TanStackRouterVite({target: 'react', autoCodeSplitting: true}),
-        react(),
-        tailwindcss()
-    ],
-})
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react(),
+    tailwindcss(),
+    flowbiteReact(),
+  ],
+});
